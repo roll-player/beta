dd67519f46ae0a10216ca12a91d5c46950f06109
Michael Sarchet <mikesarchet@gmail.com>
2016-12-11 10:00:51 -0800

    Updating CSS

--- src/site/components/header.js
+++ src/site/components/header.js
@@ -9,23 +9,25 @@ class Header extends React.Component {
   render () {
     return (
       <div styleName='nav--container'>
-      <Row styleName='header--header-row'>
-        <Col sm='1/6'>
-          <div styleName='header--header-logo'>Roll Player</div>
-        </Col>
-        <Col sm='2/3' styleName='header--header-nav'>
-          <div styleName='header--header-navitem'>
-            <Link to='/roll'>Roll Dice</Link>
-          </div>
-          <div styleName='header--header-navitem'>
-            <Link to='/spells'>Spells</Link>
-          </div>
-          <div styleName='header--header-navitem'>
-            <Link to='/about'>About</Link>
-          </div>
-        </Col>
-        <Col sm='1/6'/>
-      </Row>
+        <Row styleName='header--header-row'>
+          <Col sm='1/6'>
+              <div styleName='header--header-logo'>
+                <Link to='/'>Roll Player</Link>
+              </div>
+          </Col>
+          <Col sm='2/3' styleName='header--header-nav'>
+            <div styleName='header--header-navitem'>
+              <Link to='/roll'>Roll Dice</Link>
+            </div>
+            <div styleName='header--header-navitem'>
+              <Link to='/spells'>Spells</Link>
+            </div>
+            <div styleName='header--header-navitem'>
+              <Link to='/about'>About</Link>
+            </div>
+          </Col>
+          <Col sm='1/6'/>
+        </Row>
       </div>
     )
   }
